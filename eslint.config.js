c402e2f9aa8f466d934ce6c6310319adb7e4c677
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-06-06 14:51:08 +0100

    build: update ESLint config and dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,39 @@
-import markdown from "eslint-plugin-markdown";
-import prettier from "eslint-plugin-prettier";
-import eslintConfigPrettier from "eslint-config-prettier";
+import mdx from 'eslint-plugin-mdx'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
-/**
- * @type {import('eslint').Linter.Config}
- */
 export default [
-    ...markdown.configs.recommended,
-    {
-        // Enable the Markdown processor for all .mdx files.
-        files: ["**/*.mdx"],
-        processor: "markdown/markdown",
+  {
+    ...mdx.flat,
+    // optional, if you want to lint code blocks at the same
+    processor: mdx.createRemarkProcessor({
+      lintCodeBlocks: true,
+      // optional, if you want to disable language mapper, set it to `false`
+      // if you want to override the default language mapper inside, you can provide your own
+      languageMapper: {},
+    }),
+    rules: {
+      ...mdx.flat.rules,
+      /**
+       * Disabled to allow for Mintlify JSX snippets, which are not expressions.
+       * 
+       * @example the following code snippet is valid Mintlify MDX
+       * 
+       * ```mdx
+       * import ApikeyWarning from "/snippets/apikey-warning.mdx";
+       * 
+       * <ApikeyWarning />
+       * ```
+       */
+      'no-unused-expressions': 'off',
+      "prettier/prettier": [
+        "error",
+        {
+          // Tells prettier to use `mdx` parser for .mdx files
+          "parser": "mdx",
+        }
+      ]
     },
-    {
-        // Enable Prettier for all JavaScript and Markdown files.
-        files: ["**/*.js", "**/*.mdx"],
-        plugins: {
-            prettier: prettier,
-        },
-        rules: {
-            "prettier/prettier": "error",
-        },
-    },
-    eslintConfigPrettier,
-];
+  },
+  mdx.flatCodeBlocks,
+  eslintPluginPrettierRecommended,
+]
