134a3620899544e5a9cdbd7cff5728ff7a77a3e5
Dima Grossman <dima@grossman.io>
2025-02-11 11:06:26 +0200

    Update route.tsx

--- src/app/docs-og/[...slug]/route.tsx
+++ src/app/docs-og/[...slug]/route.tsx
@@ -1,11 +1,10 @@
-import { generateOGImage } from 'fumadocs-ui/og';
-import { metadataImage } from '../../../lib/metadata';
-
+import { generateOGImage } from "fumadocs-ui/og";
+import { metadataImage } from "../../../lib/metadata-image";
 export const GET = metadataImage.createAPI((page) => {
   return generateOGImage({
     title: page.data.title,
     description: page.data.description,
-    site: 'My App',
+    site: "Novu Documentation",
   });
 });
 
