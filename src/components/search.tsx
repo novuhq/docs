d60b5d9e14f0dbbaf3524f96b3b7fdd49fe421e4
Dima Grossman <dima@grossman.io>
2025-02-16 18:10:59 +0200

    fix: mapping

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,33 +1,22 @@
 "use client";
 
 import { OramaSearchBox } from "@orama/react-components";
-import { OramaClient } from "@oramacloud/client";
 import type { SharedProps } from "fumadocs-ui/components/dialog/search";
-
-const client = new OramaClient({
-  api_key: process.env.NEXT_PUBLIC_ORAMA_API_KEY!,
-  endpoint: "https://cloud.orama.run/v1/indexes/novu-docs-z1d208",
-});
-
-const searchBoxConfig = {
-  resultsMap: {
-    path: "url",
-    title: "title",
-    description: "description",
-  },
-  colorScheme: "system",
-  themeConfig: {},
-};
 
 export default function CustomSearchDialog(props: SharedProps) {
   return (
     <OramaSearchBox
       open={props.open}
+      onModalClosed={() => props.onOpenChange(false)}
+      resultMap={{
+        path: "url",
+        title: "title",
+        description: "content",
+      }}
       index={{
         endpoint: "https://cloud.orama.run/v1/indexes/novu-docs-z1d208",
         api_key: process.env.NEXT_PUBLIC_ORAMA_API_KEY!,
       }}
-      {...searchBoxConfig}
     />
   );
 }
