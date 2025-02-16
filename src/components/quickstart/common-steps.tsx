1ef55b1abfb9cf1aa9d284ff634e8fdfa36210a0
Dima Grossman <dima@grossman.io>
2025-02-16 13:16:59 +0200

    fix: quick starters

--- src/components/quickstart/common-steps.tsx
+++ src/components/quickstart/common-steps.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export function CreateAccountStep() {
   return (
     <Step title="Create a Novu account">
-      If you don't have a Novu account, you can create one{" "}
+      If you don&apos;t have a Novu account, you can create one{" "}
       <Link href="https://dashboard-v2.novu.co/auth/sign-up">here</Link>.
     </Step>
   );
@@ -17,6 +17,27 @@ export function BuildWorkflowStep() {
       Navigate through the sections below to create your first custom workflow.
       <Button color="secondary" size="sm" className="h-8 px-4">
         Create a workflow
+      </Button>
+    </Step>
+  );
+}
+
+export function CreateSubscriberStep() {
+  return (
+    <Step title="Create your first subscriber">
+      Novu will automatically create a subscriber when a new `subscriberId` was
+      detected from the `Inbox` component props.
+    </Step>
+  );
+}
+
+export function TriggerNotificationStep() {
+  return (
+    <Step title="Trigger your first notification">
+      Now you have setup the Notification Center and created a subscriber,
+      it&apos;s time to trigger your first notification workflow. <br />
+      <Button color="primary" className="h-9 mt-4">
+        Visit Dashboard
       </Button>
     </Step>
   );
