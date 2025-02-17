eef60a9c8308783b16409ea2d30906eef10af0d1
Dima Grossman <dima@grossman.io>
2025-02-17 11:31:06 +0200

    fix: test

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,12 @@ const config = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('typescript');
+    }
+    return config;
+  },
 };
 
 export default withMDX(config);
