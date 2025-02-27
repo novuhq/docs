082e3c6923ae925dbe6b0e7958a4c2319e12bf7c
Dima Grossman <dima@grossman.io>
2025-02-27 17:25:48 +0200

    fix: push

--- source.config.ts
+++ source.config.ts
@@ -61,11 +61,14 @@ export default defineConfig({
         transformerTwoslash(),
         {
           name: 'transformers:remove-notation-escape',
-          code(hast) {
+          // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+          code(hast: any) {
             for (const line of hast.children) {
               if (line.type !== 'element') continue;
 
-              const lastSpan = line.children.findLast((v) => v.type === 'element');
+              const lastSpan = line.children.findLast(
+                (v: { type: string }) => v.type === 'element'
+              );
 
               const head = lastSpan?.children[0];
               if (head?.type !== 'text') return;
@@ -73,7 +76,8 @@ export default defineConfig({
               head.value = head.value.replace(/\[\\!code/g, '[!code');
             }
           },
-        },
+          // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+        } as any,
       ],
     },
     remarkPlugins: [
