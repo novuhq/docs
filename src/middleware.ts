3d4439861ba6bd644c1ae809b82e8f30e78d40e0
Dima Grossman <dima@grossman.io>
2025-03-24 19:12:50 +0100

    fox: static

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import { NextResponse } from 'next/server';
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/((?!_next|robots.txt|sitemap.xml|llms.txt|static.json|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
