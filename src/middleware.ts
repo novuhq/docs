b111a72b506dc7d91460898baf0464bf46f63c72
Dima Grossman <dima@grossman.io>
2025-03-24 17:54:00 +0100

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import { NextResponse } from 'next/server';
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|txt|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
