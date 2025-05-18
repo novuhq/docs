36ec5acabc3822820ad1965b99a6be3b30e1dd31
Pawan Jain <jainpawan211199@gmail.com>
2025-05-18 16:14:03 +0530

    fix: api reference structure

---
+++ content/docs/api-reference/api-types.ts
@@ -0,0 +1,24 @@
+import {
+  ISubscriber,
+  IChannelSettings,
+  ITopic,
+  IMessage,
+  INotification,
+  IIntegration,
+} from '@novu/shared';
+
+// subscriber
+export type Subscriber = ISubscriber;
+export type ChannelSettings = IChannelSettings;
+
+// topic
+export type Topic = ITopic;
+
+// message
+export type Message = IMessage;
+
+// notification
+export type Notification = INotification;
+
+// integration
+export type Integration = IIntegration;
