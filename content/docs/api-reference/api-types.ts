0279dbb3540961acc4dac44e63bbbde1c1e8ea68
Pawan Jain <jainpawan211199@gmail.com>
2025-05-19 20:38:02 +0530

    feat: added more types

--- content/docs/api-reference/api-types.ts
+++ content/docs/api-reference/api-types.ts
@@ -1,10 +1,16 @@
 import {
   ISubscriber,
   IChannelSettings,
-  ITopic,
+  TopicDto,
   IMessage,
-  INotification,
   IIntegration,
+  ICredentials,
+  IPreviousStepFilterPart,
+  INotificationTemplate,
+  IActor,
+  IMessageCTA,
+  IActivity,
+  IActivityJob,
 } from '@novu/shared';
 
 // subscriber
@@ -12,13 +18,19 @@ export type Subscriber = ISubscriber;
 export type ChannelSettings = IChannelSettings;
 
 // topic
-export type Topic = ITopic;
+export type Topic = TopicDto;
 
 // message
 export type Message = IMessage;
+export type NotificationTemplate = INotificationTemplate;
+export type Actor = IActor;
+export type MessageCTA = IMessageCTA;
 
 // notification
-export type Notification = INotification;
+export type Notification = IActivity;
+export type ActivityJob = IActivityJob;
 
 // integration
 export type Integration = IIntegration;
+export type Credentials = ICredentials;
+export type PreviousStepFilterPart = IPreviousStepFilterPart;
