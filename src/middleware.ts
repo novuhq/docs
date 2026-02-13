0d54d35d9241bce2fe898a14fe321348127a689c
Victor Yakubu <57350321+Aviatorscode2@users.noreply.github.com>
2026-02-13 09:35:22 +0100

    Content for the Add and configure steps section. (#1044)
    * Content for the Add and configure steps section.
    
    * Add more content to the section
    
    * Update content for the index page and add redirects
    
    * fix images
    
    * fix: wordings and add todo
    
    ---------
    
    Co-authored-by: Pawan Jain <jainpawan211199@gmail.com>

--- src/middleware.ts
+++ src/middleware.ts
@@ -80,10 +80,16 @@ export default async function middleware(request: NextRequest, event: NextFetchE
     '/platform/inbox/react/headless': '/platform/inbox/headless-mode',
     '/platform/inbox/react/localization': '/platform/inbox/advanced-concepts/localization',
 
-    '/platform/workflow/tags': '/platform/workflow/configure-workflow#tags',
     '/platform/workflow/build-a-workflow': '/platform/workflow/create-a-workflow',
     '/platform/workflow/build-a-workflow#manage-payload-schema':
       '/platform/workflow/configure-workflow#payload-schema',
+    '/platform/workflow/delay': '/platform/workflow/add-and-configure-steps#delay',
+    '/platform/workflow/digest': '/platform/workflow/add-and-configure-steps#digest',
+    '/platform/workflow/throttle-step': '/platform/workflow/add-and-configure-steps#throttle',
+    '/platform/workflow/step-conditions':
+      '/platform/workflow/add-and-configure-steps/step-conditions',
+    '/platform/workflow/channel-steps': '/platform/workflow/add-and-configure-steps#channel-steps',
+    '/platform/workflow/tags': '/platform/workflow/configure-workflow#tags',
   };
 
   if (pathname in redirectMap) {
